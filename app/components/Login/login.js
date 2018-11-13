0809b142cb4fcdf2ce13b03fe1a1d0d97b5d95bd
vischugh <vischugh@gmail.com>
2018-11-13 18:48:17 +0000

    JS post request changes

--- app/components/Login/login.js
+++ app/components/Login/login.js
@@ -1,31 +1,59 @@
 "use strict";
 
-function register(){
-  var emailField = document.getElementById("user-mail");
-  var email = emailField.value;
-  var nameField = document.getElementById("user-name");
-  var name = nameField.value;
-  var passField = document.getElementById("user-pass");
-  var pass = passField.value;
-  var mobileField = document.getElementById("user-mob");
-  var mobile = mobileField.value;
-  var user = {
-    "user-mail" : email,
-    "user-name" : name,
-    "user-pass": pass,
-    "user-mob": mobile
-  };
+function register() {
+    var emailField = document.getElementById("user-mail");
+    var email = emailField.value;
+    var nameField = document.getElementById("user-name");
+    var name = nameField.value;
+    var passField = document.getElementById("user-pass");
+    var pass = passField.value;
+    var mobileField = document.getElementById("user-mob");
+    var mobile = mobileField.value;
+    var user = {
+        "user-mail": email,
+        "user-name": name,
+        "user-pass": pass,
+        "user-mob": mobile
+    };
+    postRequest("http://127.0.0.1:5000/register", user, "verification.html?email=" + email);
+}
 
-  var registerData = $.ajax({
-    type: "POST",
-    contentType: "application/json;",
-    url: "http://127.0.0.1:5000/register",
-    data: JSON.stringify(user),
-    success: function(result){
-      alert("Registered successfully");
-      location.href = "/verification.html";
-    }
-  });
+function login() {
+    var emailField = document.getElementById("user-mail");
+    var email = emailField.value;
+    var passField = document.getElementById("user-pass");
+    var mobile = mobileField.value;
+    var user = {
+        "user-mail": email,
+        "user-pass": pass
+    };
 
-  registerData.error(function(){alert("Something went wrong");})
-  }
+    postRequest("http://127.0.0.1:5000/login", user, "/verification.html");
+}
+
+function verify() {
+    var otpField = document.getElementById("otp");
+    var otp = otpField.value;
+    var params = (new URL(document.location)).searchParams;
+    var email = params.get("email");
+    var user = {
+        "user-mail": email,
+        "otp": otp
+    };
+
+    postRequest("http://127.0.0.1:5000/verify", user, "/verification.html");
+}
+
+function postRequest(postUrl, userData, nextPageUrl) {
+    var response = $.ajax({
+        type: "POST",
+        contentType: "application/json;",
+        url: postUrl,
+        data: JSON.stringify(userData),
+        success: function(result) {
+            location.href = nextPageUrl;
+        }
+    });
+
+    response.error(function() {})
+}
