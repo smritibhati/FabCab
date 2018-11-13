637aefb6518685e384fc51e48103cb42609bb090
yinsain <ydeep18@gmail.com>
2018-11-13 15:30:21 +0530

    added json stringify

--- app/components/Login/login.js
+++ app/components/Login/login.js
@@ -1,1 +1,31 @@
 "use strict";
+
+function register(){
+  var emailField = document.getElementById("user-mail");
+  var email = emailField.value;
+  var nameField = document.getElementById("user-name");
+  var name = nameField.value;
+  var passField = document.getElementById("user-pass");
+  var pass = passField.value;
+  var mobileField = document.getElementById("user-mob");
+  var mobile = mobileField.value;
+  var user = {
+    "user-mail" : email,
+    "user-name" : name,
+    "user-pass": pass,
+    "user-mob": mobile
+  };
+
+  var registerData = $.ajax({
+    type: "POST",
+    contentType: "application/json;",
+    url: "http://127.0.0.1:5000/register",
+    data: JSON.stringify(user),
+    success: function(result){
+      alert("Registered successfully");
+      location.href = "/verification.html";
+    }
+  });
+
+  registerData.error(function(){alert("Something went wrong");})
+  }
